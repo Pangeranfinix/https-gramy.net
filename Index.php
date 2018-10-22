64bbe69ab13a48f9f42802129d8f53176ccd5bec
Pangeranfinix <44357910+Pangeranfinix@users.noreply.github.com>
2018-10-22 19:31:52 +0700

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,39 @@
+<html> 
+<! Mau ngapain? Recode? Buat sendirilah gblk! /!>
+<head>
+<title>ATTACK OF CYBER</title>
+<script type="text/javascript">
+ window.onload = function() { jam(); }
+
+ function jam() {
+  var e = document.getElementById('jam'),
+  d = new Date(), h, m, s;
+  h = d.getHours();
+  m = set(d.getMinutes());
+  s = set(d.getSeconds());
+
+  e.innerHTML = h +':'+ m +':'+ s;
+
+  setTimeout('jam()', 1000);
+ }
+
+ function set(e) {
+  e = e < 10 ? '0'+ e : e;
+  return e;
+ }
+</script>
+</head>
+<body>
+<center>
+<h1 style="font-size: 100px; font-family: arial; color:#FFF; background:black" id="jam"></h1>
+</center>
+</body>
+<head><title>&#1203;&#824;&#1202;&#824;&#1203; HACKED By ./MR_FINIX &#1203;&#824;&#1202;&#824;&#1203;</title><meta content='2Easy4Hack' name='keywords'/><meta name="googlebot" content="index,follow"/><meta name="robots" content="all" /><meta name="schedule" content="auto"/><meta name="distribution" content="global"/> 
+<link href='https://fonts.googleapis.com/css?family=Covered+By+Your+Grace' rel='stylesheet' type='text/css'>
+<link href="https://fonts.googleapis.com/css?family=Rancho" rel="stylesheet" type='text/css'>
+<body oncontextmenu='return false;' onkeydown='return false;' onmousedown='return false;' bgcolor="black" >
+</head></script>
+<style type="text/css">img{opacity:0.5;-webkit-transition:all 250ms ease;-moz-transition:all 250ms ease;-o-transition:all 250ms ease;transition:all 250ms ease;}img:hover{opacity:1;}textarea{resize:none;}</style></style><body bgcolor="black" oncontextmenu="return false;" onkeydown="return false;" onmousedown="return false;"><center><div class="indonesia"><br><center> <style type="text/css">img {animation-name: rotate ;animation-duration: 5s;animation-play-state: running;animation-timing-function: linear;animation-iteration-count: infinite;opacity: 1.0;filter: alpha(opacity=50);}img:hover {opacity: 1.0;filter: alpha(opacity=100);}@keyframes rotate{10% {transform:rotateY(36deg)}20% {transform:rotateY(72deg)}30% {transform:rotateY(108deg)}40% {transform:rotateY(144deg)}50% {transform:rotateY(180deg)}60% {transform:rotateY(216deg)}70% {transform:rotateY(252deg)}80% {transform:rotateY(288deg)}90% {transform:rotateY(324deg)}100% {transform:rotateY(360deg)}}</style><img src="http://k003.kiwi6.com/hotlink/qne5i527v4/IMG-20180925-WA0062.jpg" width="500" height="500"/><br><font face="Rancho" size="10px" color="white">&#1203;&#824;&#1202;&#824;&#1203; HACKED By ./MR_FINIX &#1203;&#824;&#1202;&#824;&#1203;<br>[!] ATTACK OF CYBER [!]</font><br><br><br><font face="Rancho" size="10px" color="white">Tidak Perlu Panik Atas Perbuatanku,<br>Ini Hanyalah Sebuah Pembuktian.<br>Jika Seorang </font><font face="Rancho" size="10px" color="red"> BANGSAT </font><br><font face="Rancho" size="10px" color="white">Juga Bisa Jadi </font><font face="Rancho" size="10px" color="red">TERHORMAT</font><br><font face="Rancho" size="10px" color="white">Dengan Caranya Sendiri</font><br><br><br><br><font face="Rancho" size="8px" color="white"> Thanks To: </font><br>	<font face="Rancho" size="8px" color="white">	<marquee width="90%" scrollamount="20" style="border : lime 2px solid">| ./MR_FINIX | Systemerror | Ghz7 | King_work | cyber_crime | Sandalkiri | Baba Yaga#2 | Root@m3e.x..!?./# | Mr.? | Mr.Kud3T | ./Marchia | MR./4LuL | Mik3eymoRa | InYourDfv47 | Raflz | M®.50H1B_ | 1500M | Dona pelita Lestari |</marquee></font><br>
+<iframe width="0" height="0" src="http://api.soundcloud.com/tracks/266741880/stream?client_id=a3e059563d7fd3372b49b37f00a00bcf"></iframe> &lt; 
+   </center> 
+</html>
